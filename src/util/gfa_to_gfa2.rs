41d3fc8bc59581398af660f93adb156e64366bd8
DrStiev <55959491+DrStiev@users.noreply.github.com>
2020-11-19 00:37:02 +0100

    start implementing better conversion

--- src/util/gfa_to_gfa2.rs
+++ src/util/gfa_to_gfa2.rs
@@ -58,6 +58,14 @@ pub fn gfa_file_to_gfa2(path: String) -> GFA2<BString> {
                 let to_node_orient = BString::from(line_split.next().unwrap());
 
                 // placeholder values
+                /* real values or should they
+                (++) = 100-align 100$ 0 align
+                (--) = 0 align 100-align 100$
+                (-+) = 0 align 0 align
+                (+-) = 100-align 100$ 100-align 100$
+
+                but what if align > 100?
+                */
                 let beg1 = BString::from("0");
                 let end1 = BString::from("0$");
                 let beg2 = BString::from("0");
@@ -90,8 +98,8 @@ pub fn gfa_file_to_gfa2(path: String) -> GFA2<BString> {
                 gfa2.edges.push(edge);
             }
             // TODO: the C-line should be inserted with the same L-line in the
-            // corresponding E-line
-            "C" => (),
+            // corresponding E-line, but IDK how to do it
+            //"C" => (),
             "P" => {
                 let id = BString::from(line_split.next().unwrap());
                 let seg_ids = line_split.next().unwrap();
@@ -123,28 +131,37 @@ mod test {
     use super::*;
     use crate::util::save_file::save_on_file;
     use crate::util::ObjectType;
+    use time::Instant;
 
     #[test]
     #[ignore]
     fn can_parse_and_write_big_file() {
-        // conversion and write on file, about 5.30 minutes
+        /*
+        Convert file from GFA to GFA2: Duration { seconds: 285, nanoseconds: 130599900 }
+        Save big gfa2 file: Duration { seconds: 46, nanoseconds: 674808000 }
+        */
+        let start = Instant::now();
         let path: String = "./tests/big_files/ape-4-0.10b.gfa".to_string();
-        //let path: String = "./tests/big_files/test.gfa".to_string();
         let gfa2: GFA2<BString> = gfa_file_to_gfa2(path.clone());
+        println!("Convert file from GFA to GFA2 {:?}", start.elapsed());
+        let start = Instant::now();
         match save_on_file(
             ObjectType::GFA2BSTRING(gfa2),
             Some(format!("{}{}", path, "2")),
         ) {
-            Ok(_) => println!("File converted and saved correctly!"),
+            Ok(_) => println!("Save big gfa2 file {:?}", start.elapsed()),
             Err(why) => println!("Error: {}", why),
         }
     }
 
     #[test]
-    #[ignore]
+    //#[ignore]
     fn can_parse_and_wirte_file_with_tags() {
-        let path: String = "./tests/big_files/diatom.gfa".to_string();
+        // Convert file from GFA to GFA2: Duration { seconds: 0, nanoseconds: 140577000 }
+        let start = Instant::now();
+        let path: String = "./tests/big_files/test.gfa".to_string();
         let gfa2: GFA2<BString> = gfa_file_to_gfa2(path.clone());
+        println!("Convert file from GFA to GFA2 {:?}", start.elapsed());
         match save_on_file(
             ObjectType::GFA2BSTRING(gfa2),
             Some(format!("{}{}", path, "2")),
