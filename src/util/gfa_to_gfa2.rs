7db4f8642f168b7369c3bc293f1932daab8d3bac
DrStiev <55959491+DrStiev@users.noreply.github.com>
2020-11-19 20:22:12 +0100

    extract more information from L-lines, still no info from C- lines

--- src/util/gfa_to_gfa2.rs
+++ src/util/gfa_to_gfa2.rs
@@ -1,4 +1,4 @@
-use bstr::BString;
+use bstr::{BString, ByteSlice};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -9,20 +9,35 @@ use crate::gfa::gfa2::{Edge, GroupO, Header, Segment, GFA2};
 /// ignoring all the others.
 /// WIP
 pub fn gfa_file_to_gfa2(path: String) -> GFA2<BString> {
-    let mut gfa2 = GFA2::new();
+    let mut gfa2 = GFA2::default();
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
 
-    for (_index, line) in reader.lines().enumerate() {
+    for line in reader.lines() {
         let line = line.unwrap();
         let mut line_split = line.split_whitespace();
         let prefix = line_split.next().unwrap();
 
         match prefix {
             "H" => {
-                // default header
-                // ignore tag fields
-                gfa2.headers = vec![Header::new(b"VN:Z:2.0", b"")];
+                let mut tag = line_split.next();
+                let mut opt_fields: Vec<&[u8]> = vec![];
+                while tag.is_some() {
+                    opt_fields.push(tag.unwrap().as_bytes());
+                    tag = line_split.next();
+                }
+                let tag = opt_fields
+                    .into_iter()
+                    .map(BString::from)
+                    .collect::<BString>();
+
+                let header = Header {
+                    version: tag,
+                    tag: BString::from(""),
+                };
+                //gfa2.headers = vec![Header::new(b"VN:Z:2.0", b"")];
+                //gfa2.headers.insert(index, header);
+                gfa2.headers.push(header);
             }
             "S" => {
                 let id = BString::from(line_split.next().unwrap());
@@ -47,6 +62,7 @@ pub fn gfa_file_to_gfa2(path: String) -> GFA2<BString> {
                     tag,
                 };
                 gfa2.segments.push(segment);
+                //gfa2.segments.insert(index, segment);
             }
             "L" => {
                 // placeholder value
@@ -56,22 +72,45 @@ pub fn gfa_file_to_gfa2(path: String) -> GFA2<BString> {
                 let from_node_orient = BString::from(line_split.next().unwrap());
                 let to_node = BString::from(line_split.next().unwrap());
                 let to_node_orient = BString::from(line_split.next().unwrap());
+                let alignment = BString::from(line_split.next().unwrap());
 
                 // placeholder values
-                /* real values or should they
-                (++) = 100-align 100$ 0 align
-                (--) = 0 align 100-align 100$
-                (-+) = 0 align 0 align
-                (+-) = 100-align 100$ 100-align 100$
+                let mut beg1 = BString::from("0");
+                let mut end1 = BString::from("0$");
+                let mut beg2 = BString::from("0");
+                let mut end2 = BString::from("0$");
 
-                but what if align > 100?
-                */
-                let beg1 = BString::from("0");
-                let end1 = BString::from("0$");
-                let beg2 = BString::from("0");
-                let end2 = BString::from("0$");
+                if alignment != "*" {
+                    /* real values or should they
+                    (++) = 100-align 100$ 0 align
+                    (--) = 0 align 100-align 100$
+                    (-+) = 0 align 0 align
+                    (+-) = 100-align 100$ 100-align 100$
+                    */
+                    let len = alignment.len() - 1;
+                    let dist = alignment[..len].to_str().unwrap().parse::<i64>().unwrap();
 
-                let alignment = BString::from(line_split.next().unwrap());
+                    if from_node_orient == "+" && to_node_orient == "+" {
+                        let x = (100 - dist).abs();
+                        beg1 = BString::from(x.to_string());
+                        end1 = BString::from("100$");
+                        end2 = BString::from(dist.to_string());
+                    } else if from_node_orient == "-" && to_node_orient == "-" {
+                        let x = (100 - dist).abs();
+                        end1 = BString::from(dist.to_string());
+                        beg2 = BString::from(x.to_string());
+                        end2 = BString::from("100$");
+                    } else if from_node_orient == "-" && to_node_orient == "+" {
+                        end1 = BString::from(dist.to_string());
+                        end2 = BString::from(dist.to_string());
+                    } else if from_node_orient == "+" && to_node_orient == "-" {
+                        let x = (100 - dist).abs();
+                        beg1 = BString::from(x.to_string());
+                        end1 = BString::from("100$");
+                        beg2 = BString::from(x.to_string());
+                        end2 = BString::from("100$");
+                    }
+                }
 
                 let mut tag = line_split.next();
                 let mut opt_fields: Vec<&[u8]> = vec![];
@@ -96,10 +135,8 @@ pub fn gfa_file_to_gfa2(path: String) -> GFA2<BString> {
                     tag,
                 };
                 gfa2.edges.push(edge);
+                //gfa2.edges.insert(index, edge);
             }
-            // TODO: the C-line should be inserted with the same L-line in the
-            // corresponding E-line, but IDK how to do it
-            //"C" => (),
             "P" => {
                 let id = BString::from(line_split.next().unwrap());
                 let seg_ids = line_split.next().unwrap();
@@ -118,8 +155,9 @@ pub fn gfa_file_to_gfa2(path: String) -> GFA2<BString> {
 
                 let ogroup = GroupO::new(id, res, &tag);
                 gfa2.groups_o.push(ogroup);
+                //gfa2.groups_o.insert(index, ogroup);
             }
-            // ignore all the other lines (typically comment-lines)
+            // ignore all the other lines (typically C- and comment-lines)
             _ => (),
         }
     }
@@ -137,8 +175,8 @@ mod test {
     #[ignore]
     fn can_parse_and_write_big_file() {
         /*
-        Convert file from GFA to GFA2: Duration { seconds: 285, nanoseconds: 130599900 }
-        Save big gfa2 file: Duration { seconds: 46, nanoseconds: 674808000 }
+        Convert file from GFA to GFA2: Duration { seconds: 288, nanoseconds: 430953600 }
+        Save big gfa2 file: Duration { seconds: 69, nanoseconds: 178859800 }
         */
         let start = Instant::now();
         let path: String = "./tests/big_files/ape-4-0.10b.gfa".to_string();
@@ -157,7 +195,7 @@ mod test {
     #[test]
     //#[ignore]
     fn can_parse_and_wirte_file_with_tags() {
-        // Convert file from GFA to GFA2: Duration { seconds: 0, nanoseconds: 140577000 }
+        // Convert file from GFA to GFA2: Duration { seconds: 0, nanoseconds: 184460400 }
         let start = Instant::now();
         let path: String = "./tests/big_files/test.gfa".to_string();
         let gfa2: GFA2<BString> = gfa_file_to_gfa2(path.clone());
